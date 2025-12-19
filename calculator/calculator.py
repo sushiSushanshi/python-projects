674b5ed08897fe8172ed995e2aa7d990998149ce
Sushi Shreechand <sushisushanshi13@gmail.com>
2025-12-19 14:22:10 +0530

    create a basic cli calculator

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,1 +1,16 @@
-print("initial commit")
+import operator
+
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
+operation=input("Enter operation you want to perform on the numbers: ")
+
+operation_defination = {
+    '*':operator.mul,
+    '/':operator.truediv,
+    '-': operator.sub,
+    '+': operator.add,
+    '**': operator.pow,
+}
+
+result = operation_defination[operation](num1,num2)
+print(result)
